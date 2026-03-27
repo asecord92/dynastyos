4d8e16710c31b7b2b3b2298c5adbb3b94519fdf6
Adam Secord <ats92@cox.net>
2026-03-26 22:05:59 -0700

    fixing suspense wrapper

--- web/app/(app)/layout.tsx
+++ web/app/(app)/layout.tsx
@@ -1,12 +1,15 @@
 import { AppNav } from "../components/AppNav";
 import AuthGate from "../components/AuthGate";
+import { Suspense } from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGate>
       <div className="min-h-screen bg-gray-100 text-gray-900">
         <AppNav />
-        <div className="max-w-5xl mx-auto p-6">{children}</div>
+        <Suspense>
+          <div className="max-w-5xl mx-auto p-6">{children}</div>
+        </Suspense>
       </div>
     </AuthGate>
   );
